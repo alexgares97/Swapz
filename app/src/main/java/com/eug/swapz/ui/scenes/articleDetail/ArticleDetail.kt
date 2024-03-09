e08d02a64212f834840605eaf818c9c74405f9ef
alexgares97 <alexgares97@gmail.com>
2024-03-09 17:26:23 +0100

    gallery-09/03

--- app/src/main/java/com/eug/swapz/ui/scenes/articleDetail/ArticleDetail.kt
+++ app/src/main/java/com/eug/swapz/ui/scenes/articleDetail/ArticleDetail.kt
@@ -1,11 +1,15 @@
 package com.eug.swapz.ui.scenes.articleDetail
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 import androidx.compose.material.Text
@@ -28,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -41,7 +46,7 @@ import com.google.android.gms.location.*
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun ArticleDetail(viewModel: ArticleDetailViewModel) {
     val article = viewModel.article ?: return
@@ -54,6 +59,8 @@ fun ArticleDetail(viewModel: ArticleDetailViewModel) {
     val newLocationImg = remember { mutableStateOf(TextFieldValue()) }
     var selectedCategory = remember { mutableStateOf(TextFieldValue()) }
     var showOptions by remember { mutableStateOf(false) }
+    var onClickImg by remember { mutableStateOf(false) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -84,56 +91,55 @@ fun ArticleDetail(viewModel: ArticleDetailViewModel) {
                 .padding(innerPadding)
                 .padding(16.dp)
         ) {
-            Image(
-                painter = rememberAsyncImagePainter(article.carrusel?.get(0)),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(8.dp))
-                    .align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = article.title,
-                style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
+            if (!onClickImg) {
+                Image(
+                    painter = rememberAsyncImagePainter(article.carrusel?.get(0)),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clickable { onClickImg = true }
+                        .fillMaxWidth()
+                )
 
-            LazyRow(
-                modifier = Modifier.fillMaxWidth(),
-                contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp)
-            ) {
-                /*
-                items(article.carrusel?.size ?: 0) { index ->
-                    val imageUrl = article.carrusel?.get(index)
-                    imageUrl?.let {
+
+                Text(
+                    text = article.title,
+                    style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(bottom = 8.dp)
+                )
+
+                Text(
+                    text = article.desc,
+                    style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+                Text(
+                    text = article.value.toString() + " €",
+                    style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier.align(Alignment.Start)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(
+                    onClick = { /* Handle button click */ },
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                ) {
+                    Text(text = "Intercambiar", color = Color.White)
+                }
+            }
+            else {
+                val images = article.carrusel ?: emptyList()
+
+                val pagerState = rememberPagerState(pageCount = { images.size })
+
+                HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { page ->
+                    Box(modifier = Modifier.fillMaxSize()) {
                         Image(
-                            painter = rememberAsyncImagePainter(imageUrl),
+                            painter = rememberAsyncImagePainter(images[page]),
                             contentDescription = null,
-                            modifier = Modifier
-                                .size(200.dp)
-                                .padding(8.dp)
-                                .clip(RoundedCornerShape(8.dp))
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Fit
                         )
                     }
-                }*/
-            }
-
-            Text(
-                text = article.desc,
-                style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold),
-                modifier = Modifier.padding(vertical = 8.dp)
-            )
-            Text(
-                text = article.value.toString() + " €",
-                style = TextStyle(fontSize = 12.sp, fontWeight = FontWeight.Bold),
-                modifier = Modifier.align(Alignment.Start)
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(
-                onClick = { /* Handle button click */ },
-                modifier = Modifier.align(Alignment.CenterHorizontally)
-            ) {
-                Text(text = "Intercambiar", color = Color.White)
+                }
             }
         }
     }
--- app/src/main/java/com/eug/swapz/ui/scenes/articleDetail/ArticleDetailViewModel.kt
+++ app/src/main/java/com/eug/swapz/ui/scenes/articleDetail/ArticleDetailViewModel.kt
@@ -1,7 +1,11 @@
 package com.eug.swapz.ui.scenes.articleDetail
 import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 
 import com.eug.swapz.models.Article
 
@@ -10,10 +14,13 @@ class ArticleDetailViewModel (
     private val context: Context,
     internal val article: Article?,
 
+
     ) : ViewModel(){
     fun signOut() {
         TODO("Not yet implemented")
     }
+
+
 
 
 }
